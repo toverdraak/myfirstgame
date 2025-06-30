89723273d790b757d71ac7939b71c282e41ea169
Merlijn <merlijn.linde.vos@gmail.com>
2025-06-30 14:32:15 +0200

    Add files via upload

---
+++ Wiezen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package merlijnsmislukkingen.wiezen;
+
+/**
+ *
+ * @author merlijn
+ */
+public class Wiezen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
